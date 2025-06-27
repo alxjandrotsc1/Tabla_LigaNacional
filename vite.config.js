3cd725f30c4c9062762d9b7246c6a86e72a99dac
alxjandrotsc1 <jalejandro.torresc01@outlook.com>
2025-06-26 20:10:04 -0600

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import handlebars from 'vite-plugin-handlebars';
 import data from './data/registros.js';
 
 export default defineConfig({
+  base: process.env.DEPLOY_BASE_URL,
   plugins: [
     handlebars({
       context: data,
